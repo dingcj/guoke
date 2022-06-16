a578ad30f9d2f83a346450b8acc90acd159f5c68
dingcj <dingcj@dingcj.localdomain>
2022-06-16 14:18:19 +0000

    only build gmp.

--- build_sdk.sh
+++ build_sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Build sdk!"
 
@@ -8,5 +8,7 @@ cat GKIPCLinuxV100R001C00SPC030.tar.bz2* | tar -jvx
 
 cd GKIPCLinuxV100R001C00SPC030
 
-make build -j8
+source build/env.sh
+
+make gmp -j8
 
